f3a96d5f4798268811e10c9d126defa8d6aabcc5
AndriyTkh <151773877+AndriyTkh@users.noreply.github.com>
2024-07-14 11:08:49 +0300

    ver 1.2

--- src/tmdbMoviesAPI.js
+++ src/tmdbMoviesAPI.js
@@ -1,10 +1,20 @@
-const url =
-  "https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1";
+import axios from "axios";
 
 const options = {
   headers: {
-    // Замість api_read_access_token вставте свій токен
     Authorization:
       "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjBkODY3MTczZGI5ZDUxZjAwYzA3MDEzN2ViODUzYiIsIm5iZiI6MTcyMDc4MDEwMy41MDQ3ODEsInN1YiI6IjY2OTEwNDczMzdmZTA4ZjNjOWQ5YTM3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UwsaLb8tZmEz1WgqAoVAgEmrXJPxbfwIwMnysZZV01E",
   },
 };
+
+export function tmdbTrendingMovies() {
+  const url = `https://api.themoviedb.org/3/trending/movie/week?include_adult=false&language=en-US&page=1`;
+
+  return axios.get(url, options);
+}
+
+export function tmdbSearchMovies(query) {
+  const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+
+  return axios.get(url, options);
+}
