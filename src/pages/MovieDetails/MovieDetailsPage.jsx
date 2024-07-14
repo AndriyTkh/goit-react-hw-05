8da883430bc21da083448e9cb4ec96a86e70717a
AndriyTkh <151773877+AndriyTkh@users.noreply.github.com>
2024-07-14 13:50:57 +0300

    ver 1.3

--- src/pages/MovieDetails/MovieDetailsPage.jsx
+++ src/pages/MovieDetails/MovieDetailsPage.jsx
@@ -1,9 +1,45 @@
+import { useParams } from "react-router-dom";
+import { tmdbSearchByID } from "../../tmdbMoviesAPI";
 import css from "./MovieDetailsPage.module.css";
+import { useEffect, useState } from "react";
 
 export default function MovieDetailsPage() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function loadMovie() {
+      try {
+        const response = await tmdbSearchByID(id);
+        setMovie(response.data);
+      } catch (error) {
+        console.error("Failed to load API", error);
+      }
+    }
+
+    loadMovie();
+  }, [id]);
+
   return (
     <>
-      <div className={css.container}></div>
+      <div className={css.container}>
+        <div className={css.imageContainer}>
+          <img
+            className={css.moviePreview}
+            src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+            alt={movie.original_title}
+          />
+        </div>
+
+        <div className={css.description}>
+          <h1>{movie.original_title}</h1>
+          <p>User Score: {Math.round(movie.vote_average * 10) + "%"}</p>
+          <h2>Overview</h2>
+          <p>{movie.overview}</p>
+          <h2>Genres</h2>
+          <p></p>
+        </div>
+      </div>
     </>
   );
 }
--- src/tmdbMoviesAPI.js
+++ src/tmdbMoviesAPI.js
@@ -5,16 +5,28 @@ const options = {
     Authorization:
       "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjBkODY3MTczZGI5ZDUxZjAwYzA3MDEzN2ViODUzYiIsIm5iZiI6MTcyMDc4MDEwMy41MDQ3ODEsInN1YiI6IjY2OTEwNDczMzdmZTA4ZjNjOWQ5YTM3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UwsaLb8tZmEz1WgqAoVAgEmrXJPxbfwIwMnysZZV01E",
   },
+  params: {
+    include_adult: "false",
+    language: "en-US",
+    page: 1,
+    api_key: "920d867173db9d51f00c070137eb853b",
+  },
 };
 
 export function tmdbTrendingMovies() {
-  const url = `https://api.themoviedb.org/3/trending/movie/week?include_adult=false&language=en-US&page=1&api_key=920d867173db9d51f00c070137eb853b`;
+  const url = `https://api.themoviedb.org/3/trending/movie/week`;
 
   return axios.get(url, options);
 }
 
 export function tmdbSearchMovies(query) {
-  const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1&api_key=920d867173db9d51f00c070137eb853b`;
+  const url = `https://api.themoviedb.org/3/search/movie?query=${query}`;
+
+  return axios.get(url, options);
+}
+
+export function tmdbSearchByID(id) {
+  const url = `https://api.themoviedb.org/3/movie/${id}`;
 
   return axios.get(url, options);
 }
